54014594de92b33f13738dbccf4b0115799ca9ed
nsihor <nsihor@gmail.com>
2024-01-09 02:13:47 +0200

    add redux operations

---
+++ src/redux/operations.ts
@@ -0,0 +1,40 @@
+import axios, { AxiosResponse } from 'axios';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+axios.defaults.baseURL = 'https://fakestoreapi.com/products';
+
+export const fetchProducts = createAsyncThunk(
+    'products/fetchAll',
+    async (_, { rejectWithValue }) => {
+        try {
+            const { data }: AxiosResponse = await axios.get('/');
+            return data;
+        } catch (e: any) {
+            return rejectWithValue(e.message);
+        }
+    }
+);
+
+export const fetchProductById = createAsyncThunk(
+    'products/fetchOne',
+    async (id: number, { rejectWithValue }) => {
+        try {
+            const { data }: AxiosResponse = await axios.get(`/${id}`);
+            return data;
+        } catch (e: any) {
+            return rejectWithValue(e.message);
+        }
+    }
+);
+
+export const fetchProductByCategory = createAsyncThunk(
+    'products/fetchByCategory',
+    async (category: string, { rejectWithValue }) => {
+        try {
+            const { data }: AxiosResponse = await axios.get(`/category/${category}`);
+            return data;
+        } catch (e: any) {
+            return rejectWithValue(e.message);
+        }
+    }
+)
