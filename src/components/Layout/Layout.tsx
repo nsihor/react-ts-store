97be0bbf83c92cc787ba25735c7fb0c5365efbac
nsihor <nsihor@gmail.com>
2024-01-12 16:37:08 +0200

    add footer

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,13 +1,15 @@
-import { Outlet } from "react-router-dom";
+import {Outlet} from "react-router-dom";
 import Header from "../Header/Header";
-// import Footer from "../Footer/Footer";
+import Footer from "../Footer/Footer";
 
 const Layout = () => {
     return (
         <>
-            <Header />
-            <Outlet />
-            {/*<Footer />*/}
+            <Header/>
+            <main>
+                <Outlet/>
+            </main>
+            <Footer/>
         </>
     );
 };
